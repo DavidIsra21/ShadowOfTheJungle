158d609feddcb5f106560b7b3dacc1f26536192b
davidisra21 <davidisrael-210105@hotmail.com>
2025-04-23 12:35:51 -0600

    Arreglo de Main

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -1,14 +1,9 @@
 package Main;
 
-import java.awt.event.MouseEvent;
-
 public class MainClass {
 
     public static void main(String[] args) {
         new Game();
     }
 
-    public void mouseClicked(MouseEvent e) {
-
-    }
 }
